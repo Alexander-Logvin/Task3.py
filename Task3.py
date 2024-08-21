7a16fc0b301e19f24269a078892ecbaa9d9d3325
Alexander-Logvin <shura.logvin@yandex.ru>
2024-08-21 21:50:37 +0300

    Add files via upload

--- Task3.py
+++ Task3.py
@@ -1,7 +1,7 @@
-The_number_of_completed_homework_assignments = '12'
-Number_of_hours_spent = '1.5'
+The_number_of_completed_homework_assignments = 12
+Number_of_hours_spent = 1.5
 Course_name = 'Python'
-print(float(The_number_of_completed_homework_assignments) / float(Number_of_hours_spent))
+print((The_number_of_completed_homework_assignments) / (Number_of_hours_spent))
 print(int(8.0))
 Time_for_one_task = 8
 print('Курс:', Course_name, 'всего задач:',The_number_of_completed_homework_assignments, 'затрачено часов:',Number_of_hours_spent, 'среднее время выполнения:', Time_for_one_task)
